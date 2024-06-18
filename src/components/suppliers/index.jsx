c6e89d5efc987a7f46a2f443eef7dd3ee39a9f93
max-hubenko <maksymhub@gmail.com>
2024-06-18 18:36:41 -0500

    Fixed missing image for suppliers and white space on cart page

--- src/components/suppliers/index.jsx
+++ src/components/suppliers/index.jsx
@@ -1,11 +1,12 @@
 import './index.css'
+import supplier from "../../assets/Images/brands.jpeg"
 
 function Suppliers() {
 
     return (
         <div>
             <div id="brands-div">
-                <img id="brands" src="src/assets/Images/brands.jpeg"></img>
+                <img id="brands" src={supplier}></img>
             </div>
         </div>
     )
